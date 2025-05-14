595f1d4986f7d4116fa1bfdbde7dc2acbae39468
Leticia Evaristo <leticiasbfevaristo@gmail.com>
2025-05-14 23:19:39 +0200

    Biopython update

--- BioPython.py
+++ BioPython.py
@@ -1,2 +1,15 @@
 # In this file we explore BioPython library
 
+# PDB - proteins
+
+# Sequencing & SeqIO - genomic sequences
+
+from Bio import SeqIO
+COVID_genome = "NC_045512v2.fa"
+
+# Reading a FASTA file with SeqIO
+records = SeqIO.parse(COVID_genome, "fasta")
+for sequence in records:
+    print(f'ID: {sequence.id}, longitud: {len(sequence)}')
+
+# Align - sequences alignment
