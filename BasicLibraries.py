0bce62fff530c030ebc7e45e16fae0609f30c4b2
Leticia Evaristo <leticiasbfevaristo@gmail.com>
2025-05-17 12:09:40 +0200

    Pandas update

--- BasicLibraries.py
+++ BasicLibraries.py
@@ -22,9 +22,40 @@ import pandas as pd
 
 # Creating a DataFrame from a dictionary
 
-patients = {'Name': ['Tom', 'Ana', 'Bob', 'Mary'],
+patients_1 = {'Name': ['Tom', 'Ana', 'Bob', 'Mary'],
              'Age': [23, 36, 48, 56],
              'Diagnostic': ['Diabetes', 'Diabetes', 'Hypertension', 'Hypertension']}
 
-df_patients = pd.DataFrame(patients)
-print(df_patients)
+patients_2 = {'Name': ['Joseph', 'Claire', 'Evelyn', 'Don'],
+             'Age': [73, 29, 37, 48],
+             'Diagnostic': ['Hypothyroidism', 'Hypothyroidism', 'Diabetes', 'Hypertension']}
+
+df_patients_1 = pd.DataFrame(patients_1)
+
+df_patients_2 = pd.DataFrame(patients_2)
+
+# Merging the 2 data frames
+
+df_all_patients = pd.concat([df_patients_1, df_patients_2],
+                            ignore_index=True) # gives a new index sequence to the new dataframe
+
+print(df_all_patients)
+
+# Sorting the dataframe by a column
+
+df_all_patients = df_all_patients.sort_values(by="Name", ascending=True)
+print(df_all_patients)
+
+# List of diagnostics
+
+diagnostics = df_all_patients['Diagnostic'].unique()
+print(diagnostics)
+
+# Export a dataframe to a .csv format
+
+df_all_patients.to_csv('df_example.csv', sep='\t')
+
+# Import a .csv as a dataframe
+
+df_imported = pd.read_csv('df_example.csv', sep='\t')
+print(df_imported)
